cfe0a04a5520a85704213ed9c969d539dae24fb6
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-03-19 13:46:04 +0100

    userinput example

---
+++ src/org/campus02/nummerraten/NummerRaten.java
@@ -0,0 +1,13 @@
+package org.campus02.nummerraten;
+
+import java.util.Scanner;
+
+public class NummerRaten {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte eine Zahl eingeben: ");
+        String input = scanner.next();
+        System.out.println("input = " + input);
+    }
+}
