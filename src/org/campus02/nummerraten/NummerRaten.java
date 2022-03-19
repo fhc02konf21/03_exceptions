6a9acb607171c809e9fb41635cfd7bf02bb44a4d
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-03-19 13:53:01 +0100

    userinput till number is correct

--- src/org/campus02/nummerraten/NummerRaten.java
+++ src/org/campus02/nummerraten/NummerRaten.java
@@ -1,13 +1,22 @@
 package org.campus02.nummerraten;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class NummerRaten {
 
     public static void main(String[] args) {
+        // zufällige Zahl erzeugen lassen zwischen inkl. 0 und 10
+        int randomNumber = new Random().nextInt(10);
+        // initialer input
+        int input = -1;
+        // Scanner für Benutzereingaben erzeugen
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bitte eine Zahl eingeben: ");
-        String input = scanner.next();
-        System.out.println("input = " + input);
+
+        while(input != randomNumber) {
+            System.out.println("Bitte eine Zahl eingeben: ");
+            input = Integer.parseInt(scanner.next());
+        }
+        System.out.println("Super. Die Zahl wurde erraten");
     }
 }
